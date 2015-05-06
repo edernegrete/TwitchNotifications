4c7b9c3e0f3119b8ea032cdf047489098a73559d
Ederyairr <ederyairnglz@gmail.com>
2015-05-05 23:39:47 -0500

    Replace the game that's been playing to the title of the stream on the popup, now you getonly one notification per streamer who's live

--- background.js
+++ background.js
@@ -1,46 +1,63 @@
-function start(){
-
-var following = "https://api.twitch.tv/kraken/users/ederyairr/follows/channels?callback=?"
 var nameArray = [];
 var logoArray = [];
-var gameArray = [];
+var streamNameArray = [];
 var urlArray =[];
+var message = " is live!";
 
-$.getJSON(following)
- 	.done(function(data){
- 		for(var i = 0; i<data.follows.length; i++){
- 			var nameData = data.follows[i].channel.display_name;
- 			var live = "https://api.twitch.tv/kraken/streams/" + nameData + "?callback=?";
- 			$.getJSON(live)
- 			.done(function(data){
- 				var stream = data.stream
- 				if(stream!= null){
- 					var nameStreamer = stream.channel.display_name
- 					var gameStream = stream.channel.game;
- 					var logoStreamer = stream.channel.logo;
- 					var linkStreamer = stream.channel.link;
- 					var message = " is live!"
- 					var opt={
-				      type: 'basic',
-				      title: 'Live!',
-				      message: '',
-				      priority: 1,
-				      iconUrl: 'icon.png'
-				    };
- 					nameArray.push(nameStreamer);
- 					gameArray.push(gameStream);
- 					logoArray.push(logoStreamer);
- 					urlArray.push(linkStreamer);
-  		
- 					for(var j = 0; j<nameArray.length; j++){
+var opt={
+  type: 'basic',
+  title: 'Live!',
+  message: '',
+  priority: 1,
+  iconUrl: 'icon.png'
+};
+
+var bg = chrome.extension.getBackgroundPage();
+
+function start(){
+	var following = "https://api.twitch.tv/kraken/users/ederyairr/follows/channels?callback=?"
+
+	$.getJSON(following)
+	 	.done(function(data){
+	 		for(var i = 0; i<data.follows.length; i++){
+	 			var nameData = data.follows[i].channel.display_name;
+	 			var live = "https://api.twitch.tv/kraken/streams/" + nameData + "?callback=?";
+
+	 			$.getJSON(live)
+
+	 			.done(function(data){
+	 				var stream = data.stream
+	 				if(stream!= null){
+	 					var nameStreamer = stream.channel.display_name
+	 					var StreamName = stream.channel.status;
+	 					var logoStreamer = stream.channel.logo;
+	 					var linkStreamer = stream.channel.url;
+	 									
+	 					
+	 					if(nameArray.indexOf(nameStreamer)<0){
+	 						nameArray.push(nameStreamer);
+	 					}
+	 					if(logoArray.indexOf(logoStreamer)<0){
+	 						logoArray.push(logoStreamer);
+	 					}
+	 					if(streamNameArray.indexOf(StreamName)<0){
+	 						streamNameArray.push(StreamName);
+	 					}
+	 					if(urlArray.indexOf(linkStreamer)<0){
+	 						urlArray.push(linkStreamer);
+	 					}
+	 					for(var j=0; j<nameArray.length; j++){
 	  					opt['message'] = nameArray[j] + message;
-	  					chrome.notifications.create('', opt, function(id){console.log("Last error:", chrome.runtime.lastError);});
-
- 					}
- 				}
- 				
- 			})
- 		}
- 	});
+	  						chrome.notifications.create(nameArray[j], opt, function(){});
+						} 
+	 				}
+	 				
+	 					
+	 			})
+	 		}
+	 	});
 }
 setInterval(start,1000)
+
+
+
--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
+
 var bg = chrome.extension.getBackgroundPage();
+
+
 
 var template = document.querySelector('#myTemplate');
 for(var i = 0; i<bg.nameArray.length; i++){
 	template.content.querySelector('img').src = bg.logoArray[i];
 	template.content.querySelector('#streamerName').textContent = bg.nameArray[i];
-	template.content.querySelector('#gameName').textContent = bg.gameArray[i];
+	template.content.querySelector('#streamName').textContent = bg.streamNameArray[i];
+	template.content.querySelector('#channelLink').href=bg.urlArray[i];
+	
 	var clone = document.importNode(template.content, true);
-	document.body.appendChild(clone);		
+	document.body.appendChild(clone);
 }
+
+
 
