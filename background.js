ea9317cfc67766c58b8a54210da2005a2b04ff2b
Ederyairr <ederyairnglz@gmail.com>
2015-05-14 18:54:28 -0500

    Write your username on the extension and you'll get the notification

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
 var nameArray = [];
-var logoArray = [];
-var streamNameArray = [];
-var urlArray =[];
 var message = " is live!";
 
 var opt={
@@ -11,16 +8,17 @@ var opt={
   priority: 1,
   iconUrl: 'icon.png'
 };
-
-var bg = chrome.extension.getBackgroundPage();
-
 function start(){
-	var following = "https://api.twitch.tv/kraken/users/ederyairr/follows/channels?callback=?"
-
+	var name = localStorage.getItem('name')
+	var following = "https://api.twitch.tv/kraken/users/"+name+"/follows/channels?callback=?"
+	if(name==null){
+		return false;
+	}
+	console.log(name)
 	$.getJSON(following)
-	 	.done(function(data){
-	 		for(var i = 0; i<data.follows.length; i++){
-	 			var nameData = data.follows[i].channel.display_name;
+	 	.done(function(name){
+	 		for(var i = 0; i<name.follows.length; i++){
+	 			var nameData = name.follows[i].channel.display_name;
 	 			var live = "https://api.twitch.tv/kraken/streams/" + nameData + "?callback=?";
 
 	 			$.getJSON(live)
@@ -29,23 +27,11 @@ function start(){
 	 				var stream = data.stream
 	 				if(stream!= null){
 	 					var nameStreamer = stream.channel.display_name
-	 					var StreamName = stream.channel.status;
-	 					var logoStreamer = stream.channel.logo;
-	 					var linkStreamer = stream.channel.url;
-	 									
-	 					
+	 						 					
 	 					if(nameArray.indexOf(nameStreamer)<0){
 	 						nameArray.push(nameStreamer);
 	 					}
-	 					if(logoArray.indexOf(logoStreamer)<0){
-	 						logoArray.push(logoStreamer);
-	 					}
-	 					if(streamNameArray.indexOf(StreamName)<0){
-	 						streamNameArray.push(StreamName);
-	 					}
-	 					if(urlArray.indexOf(linkStreamer)<0){
-	 						urlArray.push(linkStreamer);
-	 					}
+
 	 					for(var j=0; j<nameArray.length; j++){
 	  					opt['message'] = nameArray[j] + message;
 	  						chrome.notifications.create(nameArray[j], opt, function(){});
--- popup.js
+++ popup.js
@@ -1,18 +1,15 @@
-
-var bg = chrome.extension.getBackgroundPage();
-
-
-
-var template = document.querySelector('#myTemplate');
-for(var i = 0; i<bg.nameArray.length; i++){
-	template.content.querySelector('img').src = bg.logoArray[i];
-	template.content.querySelector('#streamerName').textContent = bg.nameArray[i];
-	template.content.querySelector('#streamName').textContent = bg.streamNameArray[i];
-	template.content.querySelector('#channelLink').href=bg.urlArray[i];
-	
-	var clone = document.importNode(template.content, true);
-	document.body.appendChild(clone);
+var name;
+function passData () {
+	name = $('#Input').val()
+	localStorage.setItem('name', name)
+	chrome.runtime.sendMessage({type: "name", name:name});
 }
-
+if(name==''){
+	$('#Message').text('')
+	$('#Name').text('Write your username!')
+}else{
+	$('#Name').text(name)	
+}
+$('#Form').on('submit', passData)
 
 
